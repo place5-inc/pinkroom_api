f1ff26567fbdc6a8f0df95a5b0e5382d256deae9
place5lee <lee@place5.com>
2025-12-23 10:58:43 +0900

    유저 인증,회원가입

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseProvider } from 'src/libs/db';
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(private readonly db: DatabaseProvider) {}
+  async findByPhone(phone: string) {
+    return this.db
+      .selectFrom('users')
+      .selectAll()
+      .where('phone', '=', phone)
+      .executeTakeFirst();
+  }
+  async createUser(phone: string) {
+    const result = await this.db
+      .insertInto('users')
+      .values({
+        phone,
+        created_at: new Date(),
+      })
+      .executeTakeFirst();
+
+    const id = Number(result.insertId);
+
+    return this.db
+      .selectFrom('users')
+      .selectAll()
+      .where('id', '=', id)
+      .executeTakeFirst();
+  }
 }
