8e22164466855f8e9543ca9700b4bd1209667400
parkyoung1 <114381038+parkyoung1@users.noreply.github.com>
2026-01-12 16:44:13 +0900

    yml 파일 수정

--- scripts/copy-fonts.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const src = path.join(__dirname, '..', 'resources', 'fonts');
-const dest = path.join(__dirname, '..', 'dist', 'resources', 'fonts');
-
-fs.mkdirSync(dest, { recursive: true });
-fs.cpSync(src, dest, { recursive: true });
