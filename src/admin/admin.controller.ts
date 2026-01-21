3f95402db777f76c1168c28c3a3dc992729bcabf
parkyoungil <park01@place5.com>
2026-01-21 17:19:19 +0900

    알림톡 테스트 용

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -14,11 +14,13 @@ import { isEmpty } from 'src/libs/helpers';
 import { AdminService } from './admin.service';
 import { CommonService } from 'src/common/common.service';
 import { AdminBody } from 'src/libs/types';
+import { PhotoWorkerService } from 'src/photo/photo-worker.service';
 @Controller('admin')
 export class AdminController {
   constructor(
     private readonly adminService: AdminService,
     private readonly commonService: CommonService,
+    private readonly photoWorkerService: PhotoWorkerService,
   ) {}
   @Get('test')
   async test() {
@@ -171,5 +173,9 @@ export class AdminController {
       body.designId,
       body.image.id,
     );
+  }
+  @Get('fail/photo')
+  async failPhoto(@Query('userId') userId: string) {
+    this.photoWorkerService.failMakePhoto(userId, 'first');
   }
 }
