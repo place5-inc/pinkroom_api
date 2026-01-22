e27a83cb511b1ce7ecddd238c556da053f9a0f6d
place5lee <lee@place5.com>
2026-01-22 12:48:22 +0900

    재시작시 다시 만들기 시도

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -453,6 +453,22 @@ export class AdminService {
   }
   async reboot() {
     await this.messageService.sendSMSCertiCode('01054697884', 'server reboot');
+    const time = new Date(Date.now() - 20 * 60 * 1000);
+    const list = await this.db
+      .selectFrom('photo_results')
+      .where('status', '=', 'pending')
+      .where('created_at', '>', time)
+      .select('original_photo_id')
+      .distinct()
+      .execute();
+    for (const item of list) {
+      await this.messageService.sendSMSCertiCode(
+        '01054697884',
+        String(item.original_photo_id),
+      );
+      await this.workerService.makeAllPhotos(item.original_photo_id);
+    }
+
     return {
       status: HttpStatus.OK,
     };
